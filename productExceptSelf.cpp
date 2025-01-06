d8e078c72e6193d5cea7ab07f6b31c311b4b9d21
Dennis Amematekpor <dennisamematekpor@gmail.com>
2025-01-06 01:37:12 +0100

    my optimal version o of n o of n

---
+++ productExceptSelf.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int s = nums.size();
+        vector<int> prefix(s, 1);
+        for (int i = 1; i < s; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        vector<int> postfix(s, 1);
+        for (int i = s - 2; i >= 0; i--) {  // Start at s-2, not s-1
+            postfix[i] = postfix[i + 1] * nums[i + 1];
+        }
+
+        vector<int> result(s);
+        for (size_t i = 0; i < s; ++i) {
+            result[i] = prefix[i] * postfix[i];
+        }
+        return result;
+    }
+};
