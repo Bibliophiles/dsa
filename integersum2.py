efb08e09f46ff93840596876f83816c83c98e6f5
Dennis Amematekpor <dennisamematekpor@gmail.com>
2025-01-19 10:22:44 +0100

    added using two pointers

--- integersum2.py
+++ integersum2.py
@@ -9,3 +9,18 @@ class Solution:
                 return [seen[diff], i + 1]
             seen[num] = i + 1
 
+# Using two pointers wonderful code 
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+
+            if curSum > target:
+                r -= 1
+            elif curSum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
