e0eee91860ef2809ddf0ece1bd9c0aa8712f8567
Dennis Amematekpor <dennisamematekpor@gmail.com>
2025-01-19 10:11:19 +0100

    hashmap solution

---
+++ integersum2.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(numbers):
+            diff = target - num
+            if diff in seen:
+                return [seen[diff], i + 1]
+            seen[num] = i + 1
+
